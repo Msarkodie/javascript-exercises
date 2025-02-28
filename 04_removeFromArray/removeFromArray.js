46aed90129e231263ce1a97b88753622f87b3259
Sarkodie <sarkodiemichael399@gmail.com>
2025-02-28 12:23:33 +0000

    Do first half of exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array,...args) {
+let newArray=[];
+/*let newArray=array;
+for(const arg of args)
+{
+  for(let i=0;i<array.length;i++)
+{
+    if(array[i]==arg) 
+{    
+    while(newArray.includes(arg))
+{
+     newArray.splice(newArray.indexOf(arg),1);
+}
 
+}
+}
+}*/
+
+
+  array.forEach((item) => {
+   if (!args.includes(item))
+   {
+    newArray.push(item);
+   };
+ });
+return newArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
